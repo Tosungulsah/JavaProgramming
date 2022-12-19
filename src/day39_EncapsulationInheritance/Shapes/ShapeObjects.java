0963a9ded952d756961c3220314e1d829034ab6d
Gulsah Tosun <gulsahustatosun@gmail.com>
2022-12-19 10:41:40 +0000

    added new feature shape objects

--- src/day39_EncapsulationInheritance/Shapes/ShapeObjects.java
+++ src/day39_EncapsulationInheritance/Shapes/ShapeObjects.java
@@ -2,17 +2,18 @@ package day39_EncapsulationInheritance.Shapes;
 
 public class ShapeObjects {
     public static void main(String[] args) {
-        Square square= new Square(25);
-        Rectangle rectangle=new Rectangle(20,10);
-        Circle circle=new Circle(10);
+        Square square = new Square(25);
+        Rectangle rectangle = new Rectangle(20, 10);
+        Circle circle = new Circle(10);
 
 
         System.out.println(square);
         System.out.println(rectangle);
         System.out.println(circle);
 
-        rectangle.length= 5;
-        rectangle.width=2;
+        rectangle.setLength(5);
+        rectangle.setWidth(4);
         System.out.println(rectangle);
+
     }
 }
